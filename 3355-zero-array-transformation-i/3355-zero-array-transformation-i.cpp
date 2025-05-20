da2f07ab7de634d70a049d2d817f3c06c8393732
A D I T Y A <88902527+aditya9277@users.noreply.github.com>
2025-05-21 03:55:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+        int n= nums.size();
+        vector<int> diff(n,0);
+
+        //step 1 make difference array
+        for(auto &query : queries){
+            int start = query[0];
+            int end = query[1];
+
+            diff[start] +=1;
+
+            if(end+1<n){
+                diff[end]-=1;
+            }
+        }
+        
+        //step 2 compute cumulative sum
+
+        vector<int> result(n,0);
+        int cumsum=0;
+
+        for(int i=0;i<n;i++){
+            cumsum+=diff[i];
+            result[i]=cumsum;
+        }
+
+        //step 3 final check acc to question
+
+        for(int i=0;i<n;i++){
+            if(result[i]<nums[i]) return false;
+        }
+
+        return true;
+
+    }
+};
