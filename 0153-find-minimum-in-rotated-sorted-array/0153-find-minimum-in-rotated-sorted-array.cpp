72d7599f0d7ddee04608e7e1a4ff7207f3c78d40
A D I T Y A <88902527+aditya9277@users.noreply.github.com>
2025-05-24 00:58:52 +0530

    Time: 0 ms (100%), Space: 14.1 MB (48.75%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    //just find the rotation point, its the first/smallest element
+    //Mostly in questions , which have a rotated sorted array 
+    //pivot index find karna he bs
     int findpivot(vector<int>& nums, int n){
         int l=0;
         int r=n-1;
-
-        while(l<r){
+        while(l<r){ //while finding pivot , if we take l<=r , then it will be stuck in loop, so l<r.
             int mid = l+(r-l)/2;
 
-            if(nums[mid]>nums[r]) l=mid+1;
-            else r=mid;
+            if(nums[mid]>nums[r]) l=mid+1; //if nums[mid] > nums[r], means pivot will be on right
+            else r=mid; //similarly, if nums[mid]<nums[r] means pivot can be mid or in the left itself;
         }
         return nums[r];
     }
     int findMin(vector<int>& nums) {
         int n=nums.size();
         return findpivot(nums, n);
+
+        //easy boi
         
     }
 };
