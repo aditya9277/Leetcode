c098f01d5168110a88ef20be5abb3cf885cf2828
A D I T Y A <88902527+aditya9277@users.noreply.github.com>
2025-05-24 02:47:06 +0530

    Time: 0 ms (100%), Space: 12.5 MB (32.11%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n=nums.size();
-        if(nums.size()==1) return nums[0];
-        if(nums.size()==2) return nums[max(nums[0],nums[1])];
-        if(nums[0]>nums[1] ) return nums[0];
-        if(nums[n-1]>nums[n-2]) return nums[n-1];
+        // if(nums.size()==1) return 0;
+        // if(nums.size()==2) return (nums[0]>nums[1])? 0:1;
+        // if(nums[0]>nums[1] ) return 0;
+        // if(nums[n-1]>nums[n-2]) return n-1;
         int l=0;
         int r=n-1;
 
         while(l<r){
             int mid = l +(r-l)/2;
 
-            if(nums[mid]>nums[mid+1] &&  nums[mid]>nums[mid-1]) return mid;
+            // if(nums[mid]>nums[mid+1] &&  nums[mid]>nums[mid-1]) return mid;
+            //another approach can be , just skipping the above step, eventually when l==r, they will be at the peak only.
+            //so returning l or r , will give the ans, and all the edge cases will be handled automatically
 
             if(nums[mid]<nums[mid+1]) l=mid+1;
             else{
@@ -20,7 +22,7 @@ public:
             }
         }
 
-        return r;
+        return l;
         
     }
 };
