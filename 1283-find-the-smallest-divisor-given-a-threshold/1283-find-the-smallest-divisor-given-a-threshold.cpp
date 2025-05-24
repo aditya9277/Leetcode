5115a25b4d563c81e895470dfaf82183dd3777cb
A D I T Y A <88902527+aditya9277@users.noreply.github.com>
2025-05-25 01:29:11 +0530

    Time: 7 ms (74.26%), Space: 26.2 MB (17.88%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -3,12 +3,15 @@ public:
     int checkSum(vector<int>& nums, int mid){
         int sum=0;
         for(int i:nums){
+
+            //trick - use (num + d -1)/d to get the ceil value.its equivalent to ceil(num,d);
             sum+= (i+mid-1)/mid;
         }
 
         return sum;
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
         int l=1;
         int r=*max_element(nums.begin(),nums.end());
 
