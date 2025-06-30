84cc7c1537b5b13f8b1d9165af27b0304fbad76e
A D I T Y A <adisamarth20@gmail.com>
2025-07-01 03:39:18 +0530

    Time: 35 ms (90.15%), Space: 45.2 MB (76.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         //solving through prefix sum
-
-        unordered_map<int, int> mp;
+        unordered_map<int, int> mp; // to track the cumulative cum along with its count
         int res = 0;
         int cumsum=0;
-        mp.insert({0,1});
+        mp.insert({0,1}); //initially cumulative sum was zero, co counting that also as 1
 
         for(int i=0;i<nums.size();i++){
             cumsum+=nums[i];
@@ -14,7 +13,6 @@ public:
             if(mp.find(cumsum-k)!=mp.end()){
                 res+=mp[cumsum-k];                
             }
-
             mp[cumsum]++;
         }
 
