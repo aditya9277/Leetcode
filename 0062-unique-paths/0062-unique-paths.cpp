2d0ae8d88b3e7a7970de2a4117bfd42a3f116f16
A D I T Y A <adisamarth20@gmail.com>
2025-08-28 02:09:33 +0530

    Time: 0 ms (100%), Space: 9.3 MB (58.52%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> directions{{0,-1},{1,0}};
-    int find(int i, int j,int m, int n){
-        if(i>=m || j>=n) return 0;
+    int solve(int i , int j, vector<vector<int>> &dp){
+        if(i==0 && j==0 ) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
 
-        if(i==m-1 && j==n-1) return 1;
+        int up = solve(i-1, j,dp);
+        int left = solve(i, j-1,dp);
 
-        return find(i+1, j,m,n ) + find(i,j+1,m,n);
+        return dp[i][j] = up + left;
     }
     int uniquePaths(int m, int n) {
-        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-        return find(0,0,m,n);
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+
+        return solve(m-1,n-1, dp);
+        
     }
 };
